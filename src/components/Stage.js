2f6b7c1078977347ec626e75ece1118584b0de3a
free4mOriginal <free4m@gmail.com>
2019-08-16 16:47:42 -0500

    fix gameHelper

--- src/components/Stage.js
+++ src/components/Stage.js
@@ -1,12 +1,10 @@
-import React from 'react';
-import Cell from './Cell';
+import React from "react";
+import Cell from "./Cell";
 
-const Stage = ({ stage }) => {
-    return (
-        <div>
-            <Cell />
-        </div>
-    )
-}
+const Stage = ({ stage }) => (
+    <div>
+      {stage.map(row => row.map((cell, x) => <Cell key={x} type={cell[0]} />))}
+    </div>
+);
 
 export default Stage;
