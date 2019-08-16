164b90570e50feca7c04fcf7b59e9d58c62d6b5c
free4mOriginal <free4m@gmail.com>
2019-08-16 18:58:42 -0500

    style Tetris background

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,25 +1,29 @@
 import React from "react";
 
-import { createStage } from '../util/gameHelpers';
+import { createStage } from "../util/gameHelpers";
 
 // Components:
 import Stage from "./Stage";
 import Display from "./Display";
 import StartButton from "./StartButton";
 
+import { StyledTetris, StyledTetrisWrapper } from "./styles/StyledTetris";
+
 const Tetris = ({ type }) => {
   return (
-    <div>
-      <Stage stage={createStage()}/>
-      <aside>
-        <div>
-          <Display text="Score" />
-          <Display text="Rows" />
-          <Display text="Level" />
-        </div>
-        <StartButton />
-      </aside>
-    </div>
+    <StyledTetrisWrapper>
+      <StyledTetris>
+        <Stage stage={createStage()} />
+        <aside>
+          <div>
+            <Display text="Score" />
+            <Display text="Rows" />
+            <Display text="Level" />
+          </div>
+          <StartButton />
+        </aside>
+      </StyledTetris>
+    </StyledTetrisWrapper>
   );
 };
 
