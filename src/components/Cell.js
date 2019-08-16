0fd72f6f44c6f8814b6b0be77a85b024a3058a94
free4mOriginal <free4m@gmail.com>
2019-08-16 18:36:08 -0500

    add styling to Cell.js

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,11 +1,9 @@
-import React from 'react'
+import React from 'react';
+import { StyledCell } from './styles/StyledCell';
+import { TETROMINOS } from '../util/tetrominos';
 
-const Cell = ({ type }) => {
-    return (
-        <div>
-            cell
-        </div>
-    )
-}
+const Cell = ({ type }) => (
+    <StyledCell type={'L'} color={TETROMINOS['L'].color}>cell</StyledCell>
+)
 
 export default Cell;
